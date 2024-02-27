a2e662776694ee165dffc171b3131a6353346b02
Gobinda Das <gobinda993720@gmail.com>
2024-02-27 20:03:11 +0530

    add basic file with loading animation

---
+++ script.js
@@ -0,0 +1,58 @@
+let tl = gsap.timeline();
+
+
+
+// Calling functins
+loadingAnimatin();
+
+
+
+
+// Creating functions
+function loadingAnimatin() {
+    tl
+        .from('#loader h1 span', {
+            top: "110%",
+            duration: .6,
+            stagger: .1,
+            onComplete: () => {
+                count = 0;
+                const id = setInterval(() => {
+                    if (count < 100) {
+                        document.querySelector('#loader h1:first-child>span pre p').innerHTML = `${++count}`;
+                    } else {
+                        count = 100;
+                        clearInterval(id);
+
+                        tl
+                            .to("#loader h1 span, #loader>p", {
+                                opacity: 0,
+                                stagger: .05,
+                                delay: .5
+                            })
+
+                            .to("#loader", {
+                                top: "-100%",
+                                duration: 1,
+                                ease: Expo.easeInOut,
+                                onComplete: () => {
+                                    document.querySelector("#loader").remove();
+                                }
+                            });
+                    }
+                }, 30);
+                document.querySelector("#loader h1:last-child>span span:last-child").style["-webkit-animation"] = "now-anime 1.5s infinite linear";
+                document.querySelector("#loader h1:last-child>span span:last-child").style.animation = "now-anime 1.5s infinite linear";
+            }
+        })
+        .from("#loader h1:first-child>span pre", {
+            opacity: 0,
+            duration: .6,
+            delay: -.5
+        })
+        .from("#loader>p", {
+            opacity: 0,
+            duration: 1,
+            delay: -.5
+        })
+}
